2d8d067d8a7f001d03fa0e893231c9d4af1e1010
Kyle Wilson <kyle@hinterlandstills.com>
2021-04-13 13:43:19 -0500

    frontend/src: added exact paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,42 @@ import React, { Component } from 'react'
 import './App.css';
 import Home from './components/home'
 import {BrowserRouter as Router, Switch, Route } from  'react-router-dom';
+import { connect} from 'react-redux'
+import {getPlants } from './actions'
+import About from './components/about'
+import Index from './components/index'
+import Error from './components/error'
+import Form from './components/form'
+import Footer from '/components/footer'
+import Nav from '/components/nav'
 
 class App extends Component {
 
-  render(){
-    return (
+  componentDidMount(){
+    this.props.getPlants()
+  }
+
+  render() {
+    if(this.props.loading) {
+      return (
+        <h3>Loading...</h3>
+      )
+    }
+  }
+
+  return (
       <Router>
+      <Nav />
+      <Switch>
         <Route exact path="/" component={ Home }/>
-
-
-
-
-      </Router>
+        <Route exact path="/about" component={ About }/>
+        <Route exact path="/plants" component={ Index }/>
+        <Route exact path="/plants/new" component={ Form }/>
+        <Route component={ Error } />
+        </Switch>
+        <Footer />
+        </Router>
     )
   }
-}
 
 export default App
