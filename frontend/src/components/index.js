0ebece55e3c09c991206a2341258116781f8f054
Kyle Wilson <kyle@hinterlandstills.com>
2021-04-13 13:16:07 -0500

    frontend/src/components/index: create

---
+++ frontend/src/components/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import plantItem from './plantitem'
+import {deletePlant} from '../actions/index'
+
+
+class Index extends Component {
+  render() {
+    const plants = this.props.plants.map( (plant, i) =>
+    <PlantItem key={i} name={ plant.name } notes={ plant.notes } id={ plant.id } history={this.props.history}/>)
+    return (
+      <div>
+        { plants }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    plants: state.plants
+  }
+}
+
+export default connect(mapStateToProps)(Index)
