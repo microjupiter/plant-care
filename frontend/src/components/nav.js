f4d7b34aded8a4a5cbc6b049fe7069a28c33f617
Kyle Wilson <kyle@hinterlandstills.com>
2021-04-13 16:05:06 -0500

    frontend/src/components/nav: added  links

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -8,3 +8,45 @@ import InfoIcon from '@material-ui/icons/Info'
 import DoneAllIcon from '@material-ui/icons/DoneAll'
 import AddCircleIcon from '@material-ui/icons/AddCircle'
 import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions'
+
+const useStyles = makeStyles((theme) => ({
+  link: {
+    display: 'flex'
+  },
+  icon: {
+    marginRight: theme.spacing(0.5),
+    width: 20,
+    height: 20,
+  },
+  breadcrumb: {
+    margin: 'auto',
+    display: 'flex',
+    outline: 0,
+    position: 'relative',
+    justifyContent: 'center',
+  }
+}))
+
+export default function IconBreadcrumbs() {
+  const classes = useStyles()
+
+  return (
+    <Breadcrumbs aria-label='breadcrumb' className={classes.breadcrumb}>
+      <Link color='inherit' to='/' className={classes.link}>
+        Home
+      </Link>
+      <Link color='inherit' to='/about' className={classes.link}>
+        <InfoIcon className={classes.icon} />
+        About
+      </Link>
+      <Link color='inherit' to='/plants' className={classes.link}>
+        <DoneAllIcon className={classes.icon} />
+        Plants
+      </Link>
+      <Link color='inherit' to='/plants/new' className={classes.link}>
+        <AddCircleIcon className={classes.icon} />
+        Add Plant
+      </Link>
+    </Breadcrumbs>
+  )
+}
