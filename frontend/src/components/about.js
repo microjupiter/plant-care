219a5e5283fb8cd18818079ae15857f61dae5fe3
Kyle Wilson <kyle@hinterlandstills.com>
2021-04-13 11:26:31 -0500

    frontend/src/components/about: create

---
+++ frontend/src/components/about.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Plant Care!</h2>
+        <p>
+          Are you a plant lover or plant killer? Ever feel like you're committing plant genocide while everyone else you know seems to have a green thumb? Plant Care will make you feel like a magical garden gnome ready to care for your lush jungle. Before you know it, your house will feel like Jumanji!
+        </p>
+
+      </div>
+    )
+  }
+}
+
+export default About
