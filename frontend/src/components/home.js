654290e34931bc4a46c203e7e98020a41492fb04
Kyle Wilson <kyle@hinterlandstills.com>
2021-04-14 13:47:46 -0500

    altered background and aspect ratio

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import Logo from './Images/Logo.jpg'
 import Image from 'material-ui-image'
+import Desert from './Images/Desert.png'
 
 
 
-const background = Logo
+const background = Desert
+
 
 class Home extends Component {
   render() {
     return (
       <div className='App'>
-        <Image aspectRatio={(16/9)} src={background} />
+        <Image imageStyle={{width: 'inherit', height: 'inherit' }} src={background} />
       </div>
     )
   }
--- frontend/src/components/plantitem.js
+++ frontend/src/components/plantitem.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { deletePlant } from '../actions/index'
-
+import Home from './home' 
 
 
 class PlantItem extends Component {
   render() {
     const {name, species, notes, id} = this.props
     return (
-
       <div>
         <div className="container">
           <br />
@@ -17,6 +16,7 @@ class PlantItem extends Component {
           <p> {notes} </p>
           <br />
           <button className="button-color" onClick={() => this.props.deletePlant (id, this.props.history )}>Delete Plant</button> 
+          
           </div>
       </div>
     )
